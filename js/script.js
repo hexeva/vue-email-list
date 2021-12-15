224e706d6501e9ad6d0444f0ddd35ea631622752
hexeva <patrick79.g@gmail.com>
2021-12-15 15:42:40 +0100

    push

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
  Vue.config.devtools = true;
 
+//  Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
  const app = new Vue (
     {
        el: '#root',
-        data: {},
-        methods: {},
+        data: {
+            randomMail:'',
+            MailList:[],
+        },
+        methods: {
+            getRandomMail:function(){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    console.log(response);
+                
+            });
+            }
+        },
         
     }
 );
