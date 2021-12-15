e0ce1d07a8f5b5338e75348d011b1218172e83e1
hexeva <patrick79.g@gmail.com>
2021-12-15 16:33:57 +0100

    push

--- js/script.js
+++ js/script.js
@@ -17,7 +17,11 @@
             getRandomMail:function(){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((response) => {
-                    console.log(response);
+                    // console.log(response.data.response);
+                    this.randomMail = response.data.response
+                    console.log(this.randomMail);
+                    
+                  
                 
             });
             }
